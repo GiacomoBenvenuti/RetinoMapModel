1c9054b472edd6278c3504e156f723ad93761e7b
giacomox <giacomox@gmail.com>
2019-10-09 17:15:33 -0500

    Update FitRetino.m
    (1) Now you can set the inital param, (2) Fixed param A, the Scaling is free to change. (3) decreased downsampling

--- FitRetino.m
+++ FitRetino.m
@@ -1,5 +1,7 @@
-function param = FitRetino(RX,RY)
-% param = FitRetino(RX,RY)
+function param = FitRetino(varargin)
+% param = FitRetino(RX,RY) % Default initial param
+% param = FitRetino(RX,RY,initial_param) % Custom initial param
+% param = [ A, Bx, By, Angle, U0, V0 ]
 %
 %-------------------------------------------
 % by Giacomo Benvenuti & Wahiba Taouali
@@ -8,12 +10,21 @@ function param = FitRetino(RX,RY)
 % https://github.com/giacomox/RetinoMapModel
 %-------------------------------------------
 
+RX = varargin{1};
+RY = varargin{2};
+
+A = 5;
+%       R_scale   Scaling     R_spacingX  R_spacingY  R_angle  R_OffsetX  R_OffsetY 
 % Initial param
-a0 = [  5.0000  500.0488  3000.0000   60.0000  600.0000 -150.0000 ];
+a0 = [   .000001         5        1100         1100         0        19000       2400   ];
 % Param Lower Bound
-LB = [  .1  300   300    -90   -200   -200];
-% Param Upper Bound
-UB = [ 30   5000  5000   90    1000   1000];
+LB = [   .000001         3          300         300        -180       -200       -200   ];
+% Param Upper Bound 
+UB = [   .000001        10        10000        10000        180       30000      30000   ];
+
+if nargin > 2
+   a0 = varargin{3}; 
+end
 
 param=bads(@(param) cost_fun(param,RX,RY),a0,LB,UB) ;
 
@@ -22,7 +33,7 @@ end
 function d = cost_fun(param,RX,RY)
 
 % Downsample the data
-N = 20;
+N = 40;
 [h w] = size(RX) ;
 
 % Take a sample of equally spaced pixels values
@@ -30,6 +41,8 @@ q = round(linspace(1,h,N));
 k = round(linspace(1,w,N));
 
 [X,Y] = meshgrid(q,k) ;
+
+
 RXs = RX(q,k) ;
 RYs = RY(q,k) ;
 X = X(~isnan(RXs)) ;
@@ -37,11 +50,10 @@ Y = Y(~isnan(RYs) );
 RXs = RXs(~isnan(RXs)) ;
 RYs = RYs(~isnan(RYs) );
 
-
-[x y] = RetinoModel(RXs,RYs,param);
+[x y] = RetinoModel(RXs ,RYs , param);
 
 % Calculate Least mean squares error
 d = nansum(   ((X - x).^2  +  (Y - y).^2 )   ) ;
 %d = nansum( (X - x).^2 )  +  nansum( (Y - y).^2 )   ;
-
+%d = 1 - corr( pdist(cat(2,[X,Y]))', pdist(cat(2,[x,y]))' );
 end
